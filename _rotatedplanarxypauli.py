e16fe3f068d789ddce5e7417979dd029f569a4a1
adamud1 <166613484+adamud1@users.noreply.github.com>
2024-05-02 15:46:53 +0200

    Updated docstrings in _rotatedplanarxypauli.py

--- _rotatedplanarxypauli.py
+++ _rotatedplanarxypauli.py
@@ -3,16 +3,16 @@ from qecsim.models.rotatedplanar import RotatedPlanarPauli
 
 class RotatedPlanarXYPauli(RotatedPlanarPauli):
     """
-    Defines a Pauli operator on a rotated planar XZ/ZX lattice with Hadamard Y applied on the top left diagonal.
+    Defines a Pauli operator on a rotated planar XX/XX or YY/YY lattice.
     Notes:
     * This is a utility class used by rotated planar implementations of the core models.
-    * It is typically instantiated using :meth:`qecsim.models.rotatedplanarxz.RotatedPlanarXZCode.new_pauli`?
+    * It is typically instantiated using :meth:`qecsim.models.rotatedplanarxz.RotatedPlanarXYCode.new_pauli`?
     Use cases:
-    * Construct a rotated planar XZY Pauli operator by applying site, plaquette and logical operators:
+    * Construct a rotated planar XY Pauli operator by applying site, plaquette and logical operators:
       :meth:`site`, :meth:`plaquette`, :meth:`logical_x`, :meth:`logical_z`.
     * Get the single Pauli operator applied to a given site: :meth:`operator`
     * Convert to binary symplectic form: :meth:`to_bsf`.
-    * Copy a rotated planar XZ Pauli operator: :meth:`copy`.
+    * Copy a rotated planar XY Pauli operator: :meth:`copy`.
     """
 
     def plaquette(self, index):
@@ -20,13 +20,11 @@ class RotatedPlanarXYPauli(RotatedPlanarPauli):
         Apply a plaquette operator at the given index.
         Notes:
         * Index is in the format (x, y).
-        * Z operators are applied to SW and NE qubits. X operators are applied to NW and SE qubits. Z operators acting on 
-        qubits along the upper-left to lower right diagonal are switched to Y.
         * Applying plaquette operators on plaquettes that lie outside the lattice have no effect on the lattice.
         :param index: Index identifying the plaquette in the format (x, y).
         :type index: 2-tuple of int
         :return: self (to allow chaining)
-        :rtype: RotatedPlanarXZYPauli
+        :rtype: RotatedPlanarXYPauli
         """
         x, y = index
         # apply if index within lattice
@@ -47,12 +45,12 @@ class RotatedPlanarXYPauli(RotatedPlanarPauli):
 
     def logical_x(self):
         """
-        Apply a logical X operator, i.e. alternate X and Z between lower-left and lower-right corners.
+        Apply a logical X operator, i.e. X along bottow row.
         Notes:
         * X unchanged from Hadamard YZ operator
         * Operators are applied to the bottom row to allow optimisation of the MPS decoder.
         :return: self (to allow chaining)
-        :rtype: RotatedPlanarXZYPauli
+        :rtype: RotatedPlanarXYPauli
         """
         max_site_x, max_site_y = self.code.site_bounds
         self.site('X', *((x, 0) for x in range(0, max_site_x + 1)))
@@ -60,12 +58,11 @@ class RotatedPlanarXYPauli(RotatedPlanarPauli):
 
     def logical_y(self):
         """
-        Apply a logical Z operator, i.e. alternate Z and X between lower-right and upper-right corners.
+        Apply a logical Y operator, i.e. Y on right column.
         Notes:
-        * First Z operator applied on lower right qubit gets switched to Y.
         * Operators are applied to the rightmost column to allow optimisation of the MPS decoder.
         :return: self (to allow chaining)
-        :rtype: RotatedPlanarXZPauli
+        :rtype: RotatedPlanarXYPauli
         """
         max_site_x, max_site_y = self.code.site_bounds
         self.site('Y', *((max_site_x, y) for y in range(0, max_site_y + 1)))
